3fa2f9a0ed7a28aee50f7c0492530ab59323d9d2
Michel <skvarkomichal@gmail.com>
2020-04-27 12:50:22 +0200

    Read file
    Read file

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import cv2
-cap = cv2.VideoCapture(0)
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+from monovideoodometery import MonoVideoOdometery
+import os
 
-while(True):
-    ret, frame = cap.read()
-    cv2.imshow('frame',frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+img_path = 'C:\\data_odometry_gray\\dataset\\sequences\\00\\image_0\\'
+pose_path = 'C:\\data_odometry_poses\\dataset\\poses\\00.txt'
