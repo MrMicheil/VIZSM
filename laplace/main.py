2014b19229d7ca7c4bb5b54501147e6a70d57dc8
Mark <pal.mark28@gmail.com>
2020-05-10 15:26:42 +0200

    refactoring

--- laplace/main.py
+++ laplace/main.py
@@ -5,29 +5,34 @@ import matplotlib.pyplot as plt
 import edge_detect as ed
 from scipy import signal
 
+# config
+filt_ratio = 1/2
+min_max_amp = 5
 
-infile = 'resources/lena.png'
+
+infile = 'resources/face.png'
 
 rgb = ed.load_file(infile)
 gray = ed.to_gray(rgb)
 gray = ed.averageFilter(gray, 5)
 gray = ed.median_filter(gray, 3)
-gray = ed.median_filter(gray, 3)
-#gray = ed.median_filter(gray, 3)
 lap = ed.laplacian(gray)
 
 (zc, stdev) = ed.find_zero_cross(lap)
+zc_filt = np.zeros(zc.shape, np.uint8)
+zc *= 255
 vals = np.sort(stdev, axis=None)
 vals = [val for val in vals if val != 0]
 
-ind = int(3*len(vals)/4)
+ind = int(filt_ratio*len(vals) - 1)
 th = vals[ind]
 print(f'TH: {th}')
 for i in range(0, zc.shape[0]):
     for j in range(0, zc.shape[1]):
         if(stdev[i, j] > th):
-            zc[i, j] = 255
+            zc_filt[i, j] = 255
 
 ed.print_to_file(gray, 'output/gray.png')
-ed.print_to_file(lap, 'output/norm.png')
-ed.print_to_file(zc, 'output/edges.png')
+ed.print_to_file(min_max_amp*ed.set_min_max(lap), 'output/minmax.png')
+ed.print_to_file(zc_filt, 'output/edges_filt.png')
+ed.print_to_file(zc, 'output/edges_all.png')
