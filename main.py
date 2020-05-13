91af318ea5cab5d4be4c1e96d17aa242fbc4399b
Michel <skvarkomichal@gmail.com>
2020-05-13 19:50:17 +0200

    Finale
    Finalne nastavenia

--- main.py
+++ main.py
@@ -4,51 +4,38 @@ import matplotlib.pyplot as plt
 from monoodometry import MonoOdometry
 import os
 
-
-
 img_path = 'C:\\data_odometry_gray\\dataset\\sequences\\25\\image_0\\'
-pose_path = 'C:\\data_odometry_poses\\dataset\\poses\\00.txt'
-
-
+pose_path = 'C:\\data_odometry_poses\\dataset\\poses\\25.txt'
 
 # focal V5, V7, V9, V11, V13 = 187.5      V6, V8, V10 = 375
 
 #                                                                                       35/10/5   1200      32       375   
 # example: image resolution of 640x480, we compute the OpenCV focal length as follows: 35mm * (640 pixels / 32 mm) = 700 pixels
-# for blender is optimal between 350 and 3500 than for a setting of 35 (we have 35,10 and 5 :D)
+# for blender is optimal between 350 and 3500 than for a setting of 35
 
 # pp - principal point
 # double cx = (newImgSize.width)*0.5;
 # double cy = (newImgSize.height)*0.5; 
 
-
-SenzorSize = 32
-focalInMM = 10
-width = 1200
-height = 380
+#                          Video ulica (25)            Video s metrom (27)                    
+SenzorSize = 32            #32                         9.5
+focalInMM = 10             #10                         27
+width = 1200               #1200                       1024
+height = 380               #380                        768
 focal = focalInMM*(width/SenzorSize)
 pp = (width*0.5, height*0.5)
-# focal = 718.8560      pp = (607.1928, 185.2157)        dataset 00
+#Kitty focal = 718.8560      pp = (607.1928, 185.2157)        dataset 00
 
-
-# Parameters for lucas kanade optical flow
+# Definovanie parametrov pre  lucas kanade optical flow
 lk_params = dict( winSize  = (21,21),
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
 
-#wimSize  This determines the integration window size. Small windows are more sensitive to noise 
-#         and may miss larger motions. Large windows will “survive” an occlusion.
-#criteria criteria has two interesting parameters here - 
-#         - the max number (10 above) of iterations and 
-#         - epsilon (0.03 above). More iterations means a more exhaustive search, and a smaller 
-#        epsilon finishes earlier. These are primarily useful in exchanging speed vs accuracy, but mainly stay the same.
-
-Odometry = MonoOdometry(img_path, pose_path, focal, pp, lk_params)
-Cesta = np.zeros(shape=(600, 800, 3))
+Odometry = MonoOdometry(img_path, pose_path, focal, pp, lk_params) 
+Cesta = np.zeros(shape=(800, 800, 3))
 
 while(Odometry.dalsiSnimok()):
 
     Snimka = Odometry.current_frame
-
     cv.imshow('Zaznam', Snimka)
     k = cv.waitKey(1)
     if k == 27:
@@ -59,16 +46,16 @@ while(Odometry.dalsiSnimok()):
     VyratSur = Odometry.vyrataj_suradnice()
     RealneSur = Odometry.vyrataj_realne_suradnice()
 
-    #print("Chyba: ", np.linalg.norm(VyratSur - RealneSur))
+    #Vypis do terminalu
     print("x: {}, y: {}, z: {}".format(*[str(pt) for pt in VyratSur]))
-    print("Realne_x: {}, Realne_y: {}, Realne_z: {}".format(*[str(pt) for pt in RealneSur]))
- 
-    x, y, z = [int(round(x)) for x in VyratSur]
-    Cesta = cv.circle(Cesta, (x * 3 + 400, z + 100), 1, list((0, 0, 255)), 4)
-    
+    #Vypis na obrazovku
+    x, y, z = [int((x)) for x in VyratSur]
+    Cesta = cv.circle(Cesta, (x * 3 + 400, z * 3 + 100), 1, list((0, 255, 0)), 4)
+     
     x, y, z = [int(round(x)) for x in RealneSur]
-    Cesta = cv.circle(Cesta, (x * 3 + 400, z + 100), 1, list((0, 255, 0)), 4)
 
+    Cesta = cv.circle(Cesta, (x * 3 + 400, z * 3 + 500), 1, list((0, 0, 255)), 4)
+    cv.putText(Cesta, 'Vizualna odometria:', (100, 100), cv.FONT_HERSHEY_PLAIN, 1,(255,255,255), 1)
+    cv.putText(Cesta, 'Skutocna pozicia:', (100, 500), cv.FONT_HERSHEY_PLAIN, 1,(255,255,255), 1)
     cv.imshow('Trajektoria', Cesta)
 
-cv.destroyAllWindows()
--- monoodometry.py
+++ monoodometry.py
@@ -16,11 +16,12 @@ class MonoOdometry(object):
         with open(pose_file_path) as f:
             self.riadok = f.readlines()
         self.zmenaSnimku()
-
+    #Funnkcia na vratenie ID snimku
     def dalsiSnimok(self):
-         return self.id < len(os.listdir(self.file_path)) 
-        #    bool -- Boolean value denoting whether there are still frames in the folder to process
 
+         return self.id < len(os.listdir(self.file_path)) 
+
+    #Funkcia na zmenu snimku 
     def zmenaSnimku(self):
         
         if self.id < 2:
@@ -34,89 +35,90 @@ class MonoOdometry(object):
             self.odometria()
             self.id += 1
 
+    #Funkcia na detegciu hrán
     def detect(self, img):   
 
         p0 = self.detector.detect(img) 
         return np.array([x.pt for x in p0], dtype=np.float32).reshape(-1, 1, 2)
-        #    np.array -- A sequence of points in (x, y) coordinate format denoting location of detected keypoint
 
-
+    #Funkcia na vyratanie suradnic z visualnej odometrie
     def vyrataj_suradnice(self):
-
+ 
         diag = np.array([[-1, 0, 0],
                         [0, -1, 0],
                         [0, 0, -1]])
         adj_coord = np.matmul(diag, self.t)
-
         return adj_coord.flatten() 
-        # We multiply by the diagonal matrix to fix our vector onto same coordinate axis as true values
-    
+ 
+    #Funkcia na vycitanie suradnic z txt suboru ak mame dataset s poziciou framu     
     def vyrataj_realne_suradnice(self):
-        
-        riadok = self.riadok[self.id - 1].strip().split()
-        x = float(riadok[0])    #[3]
-        y = float(riadok[1])    #[7]
-        z = float(riadok[2])   #[11]
 
+        riadok = self.riadok[self.id - 1].strip().split()
+        x = float(riadok[0])
+        y = float(riadok[1])
+        z = float(riadok[2])
         realne_suradnice = np.array([[x], [y], [z]])
         return realne_suradnice.flatten()
-
+    
+    #Funkcia na vyratanie odometrie pre prvu snimku 
     def odometriaFirst(self):
 
         self.p0 = self.detect(self.old_frame)
+        #Funkcia Lucas Kanade pre zistenie totoznych hran medzi snimkami
         self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_frame, self.current_frame, self.p0, None, **self.lk_params)
         self.good_old = self.p0[st == 1]
         self.good_new = self.p1[st == 1] 
-
-        
+        #Funkcia na najdenie zakladnej matice
         E, _ = cv2.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+        #Funkcia na vyratanie R a t 
         _, self.R, self.t, _ = cv2.recoverPose(E, self.good_old, self.good_new, self.R, self.t, self.focal, self.pp, None)
         self.n_features = self.good_new.shape[0] 
         
-   
+    #Funkcia na ratanie odometrie pre vsetky ostatne snimky
     def odometria(self):
-
-        if self.n_features < 2000:                          #
+        
+        if self.n_features < 2000:                       
             self.p0 = self.detect(self.old_frame)
 
+        #Funkcia Lucas Kanade pre zistenie totoznych hran medzi snimkami
         self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_frame, self.current_frame, self.p0, None, **self.lk_params)
-        
         # Save the good points from the optical flow
         self.good_old = self.p0[st == 1]
         self.good_new = self.p1[st == 1]
-
-
+        #Funkcia na najdenie zakladnej matice
         E, _ = cv2.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+        #Funkcia na vyratanie R a t 
         _, R, t, _ = cv2.recoverPose(E, self.good_old, self.good_new, self.R.copy(), self.t.copy(), self.focal, self.pp, None)
-        print(t)
 
 
-        #absolute_scale = self.get_absolute_scale() 
-        absolute_scale = 0.4            #read document
+        #absolute_scale = self.get_absolute_scale()     <<<<====== iba ak je dostupny dataset
+        absolute_scale = self.get_absolute_scale()         #mierka pohybu kamery     
+                                    #pre ulicu je to 0.4
+                                    #pre video s metrom je to 2
         if (absolute_scale > 0.1 and abs(t[2][0]) > abs(t[0][0]) and abs(t[2][0]) > abs(t[1][0])):  #Ak som nemal dataset dal som tam len absolute_scale = 0.2 Je to udaj napr. o rychlosti kamery
-            self.t = self.t + absolute_scale * self.R.dot(t)    
+            #Rovnice na vyratanie polohy kamery
+            self.t = self.t + absolute_scale * self.R.dot(t)     
             self.R = R.dot(self.R)
 
         self.n_features = self.good_new.shape[0]
-        # Save good points
+        
 
-##########      ONLY WITH DATASET      #############
+##########      Iba ak je dostupny dataset      #############
 
+    #Funkcia na vypocitanie mierky z datasetu (scalu)
     def get_absolute_scale(self):
 
         riadok = self.riadok[self.id - 1].strip().split()
-        x_prev = float(riadok[0])   #[3]
-        y_prev = float(riadok[1])   #[7]
-        z_prev = float(riadok[2])  #[11]
+        x_prev = float(riadok[0])  
+        y_prev = float(riadok[1])   
+        z_prev = float(riadok[2])  
         riadok = self.riadok[self.id].strip().split()
-        x = float(riadok[0])        #[3]
-        y = float(riadok[1])        #[7]
-        z = float(riadok[2])        #[11]
+        x = float(riadok[0])   
+        y = float(riadok[1])
+        z = float(riadok[2])    
 
         true_vect = np.array([[x], [y], [z]])
         prev_vect = np.array([[x_prev], [y_prev], [z_prev]])
-        
-    #   alternative 
-    #   return np.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev))    
+          
         return np.linalg.norm(true_vect - prev_vect)
 
